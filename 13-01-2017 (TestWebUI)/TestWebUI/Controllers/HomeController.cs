67549bac0a04219565a4f1ed0166e9d73053b80c
Jonathan <jonathan.lake1@live.com>
2017-03-30 15:53:54 +1300

    Starting Point
    This was one of my starting points for my project. Web User Interface.

---
+++ 13-01-2017 (TestWebUI)/TestWebUI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Home page description and linkage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //About page description and linkage
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        //Contact page description and linkage
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
